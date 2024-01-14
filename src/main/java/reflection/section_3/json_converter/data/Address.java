e8043a7983edd9e7deacd328b09e849780540384
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-01-14 23:21:55 +0100

    JSON serializer example

---
+++ src/main/java/reflection/section_3/json_converter/data/Address.java
@@ -0,0 +1,11 @@
+package reflection.section_3.json_converter.data;
+
+public class Address {
+    private final String street;
+    private final short apartment;
+
+    public Address(String street, short apartment) {
+        this.street = street;
+        this.apartment = apartment;
+    }
+}
